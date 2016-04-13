b73e0a0ce01cdd8530d173babb3480f19a395674
Anton <MytnikAA@gmail.com>
2016-04-13 14:28:41 +0600

    добавить дао интерфейс

---
+++ AIS2016/src/dao/DAOInterface.java
@@ -0,0 +1,12 @@
+package dao;
+
+import java.util.List;
+
+public interface DAOInterface<T> {
+    
+    void persist(T value);
+    void delete(T value);
+    void update(T value);
+    T findById(Integer id);
+    List<T> findById();
+}
